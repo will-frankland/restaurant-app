0ed6f77ed0ac772e92266fe741c83c0537d33029
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-15 15:41:56 -0500

    add links to smallscreen section

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,14 @@
 import React from 'react';
-import { GiHamburger } from 'react-icons/gi';
+import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import images from '../../constants/images';
 
 import './Navbar.css';
 
 const Navbar = () => (
-  <div className="app__navbar">
+  <nav className="app__navbar">
     <div className='app__navbar-logo'>
       <img src={images.gericht} alt="app logo"/>
-
     </div>
     <ul className='app__navbar-links'>
       <li className='p__opensans'><a href='#home'>Home</a></li>
@@ -23,7 +22,21 @@ const Navbar = () => (
       <div />
       <a href='/' className='p__opensans'>Book A Table</a>
     </div>
-  </div>
+    <div className='app__navbar-smallscreen'>
+      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}}/>
+
+      <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+        <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => {}} />
+          <ul className='app__navbar-smallscreen-links'>
+            <li className='p__opensans'><a href='#home'>Home</a></li>
+            <li className='p__opensans'><a href='#about'>About</a></li>
+            <li className='p__opensans'><a href='#menu'>Menu</a></li>
+            <li className='p__opensans'><a href='#awards'>Awards</a></li>
+            <li className='p__opensans'><a href='#contact'>Contact</a></li>
+          </ul>
+      </div>
+    </div>
+  </nav>
 );
 
 export default Navbar;
