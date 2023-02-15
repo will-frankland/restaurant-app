771b5c061dcbd057848ef6afa8b63fc9c5808cf3
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-15 15:54:22 -0500

    add useState to toggle menu

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import { MdOutlineRestaurantMenu } from 'react-icons/md';
 import images from '../../constants/images';
@@ -6,6 +6,7 @@ import images from '../../constants/images';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false);
   return (
     <nav className="app__navbar">
       <div className='app__navbar-logo'>
@@ -24,10 +25,11 @@ const Navbar = () => {
         <a href='/' className='p__opensans'>Book A Table</a>
       </div>
       <div className='app__navbar-smallscreen'>
-        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}}/>
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => setToggleMenu(true)}/>
 
+        {toggleMenu && (
         <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
-          <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => {}} />
+          <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => setToggleMenu(false)} />
             <ul className='app__navbar-smallscreen-links'>
               <li className='p__opensans'><a href='#home'>Home</a></li>
               <li className='p__opensans'><a href='#about'>About</a></li>
@@ -36,6 +38,7 @@ const Navbar = () => {
               <li className='p__opensans'><a href='#contact'>Contact</a></li>
             </ul>
         </div>
+        )}
       </div>
     </nav>
   );
