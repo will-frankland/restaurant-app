ef3b3ffeca7b559783e8909aa6dd11ecfe2779f7
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-15 15:48:58 -0500

    transform from instant return function to reg function

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -5,38 +5,40 @@ import images from '../../constants/images';
 
 import './Navbar.css';
 
-const Navbar = () => (
-  <nav className="app__navbar">
-    <div className='app__navbar-logo'>
-      <img src={images.gericht} alt="app logo"/>
-    </div>
-    <ul className='app__navbar-links'>
-      <li className='p__opensans'><a href='#home'>Home</a></li>
-      <li className='p__opensans'><a href='#about'>About</a></li>
-      <li className='p__opensans'><a href='#menu'>Menu</a></li>
-      <li className='p__opensans'><a href='#awards'>Awards</a></li>
-      <li className='p__opensans'><a href='#contact'>Contact</a></li>
-    </ul>
-    <div className='app__navbar-login'>
-      <a href='#login' className='p__opensans'>Login / Register</a>
-      <div />
-      <a href='/' className='p__opensans'>Book A Table</a>
-    </div>
-    <div className='app__navbar-smallscreen'>
-      <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}}/>
-
-      <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
-        <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => {}} />
-          <ul className='app__navbar-smallscreen-links'>
-            <li className='p__opensans'><a href='#home'>Home</a></li>
-            <li className='p__opensans'><a href='#about'>About</a></li>
-            <li className='p__opensans'><a href='#menu'>Menu</a></li>
-            <li className='p__opensans'><a href='#awards'>Awards</a></li>
-            <li className='p__opensans'><a href='#contact'>Contact</a></li>
-          </ul>
+const Navbar = () => {
+  return (
+    <nav className="app__navbar">
+      <div className='app__navbar-logo'>
+        <img src={images.gericht} alt="app logo"/>
       </div>
-    </div>
-  </nav>
-);
+      <ul className='app__navbar-links'>
+        <li className='p__opensans'><a href='#home'>Home</a></li>
+        <li className='p__opensans'><a href='#about'>About</a></li>
+        <li className='p__opensans'><a href='#menu'>Menu</a></li>
+        <li className='p__opensans'><a href='#awards'>Awards</a></li>
+        <li className='p__opensans'><a href='#contact'>Contact</a></li>
+      </ul>
+      <div className='app__navbar-login'>
+        <a href='#login' className='p__opensans'>Login / Register</a>
+        <div />
+        <a href='/' className='p__opensans'>Book A Table</a>
+      </div>
+      <div className='app__navbar-smallscreen'>
+        <GiHamburgerMenu color="#fff" fontSize={27} onClick={() => {}}/>
+
+        <div className='app__navbar-smallscreen_overlay flex__center slide-bottom'>
+          <MdOutlineRestaurantMenu fontSize={27} className="overlay__close" onClick={() => {}} />
+            <ul className='app__navbar-smallscreen-links'>
+              <li className='p__opensans'><a href='#home'>Home</a></li>
+              <li className='p__opensans'><a href='#about'>About</a></li>
+              <li className='p__opensans'><a href='#menu'>Menu</a></li>
+              <li className='p__opensans'><a href='#awards'>Awards</a></li>
+              <li className='p__opensans'><a href='#contact'>Contact</a></li>
+            </ul>
+        </div>
+      </div>
+    </nav>
+  );
+}
 
 export default Navbar;
