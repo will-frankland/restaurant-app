26c5e7b1585486a5bde7e2c4c0a574d7fd3227f6
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-16 17:22:38 -0500

    add title and subheading Divs and then map over data to access winelist and display

--- src/container/Menu/SpecialMenu.jsx
+++ src/container/Menu/SpecialMenu.jsx
@@ -5,8 +5,23 @@ import { SubHeading, MenuItem } from '../../components';
 import './SpecialMenu.css';
 
 const SpecialMenu = () => (
-  <div>
-    SpecialMenu
+  <div className='app__specialMenu flex__center section__padding' id='menu'>
+    <div className='app__specialMenu-title'>
+      <SubHeading title="Menu that fits your palatte"/>
+      <h1 className='headtext__cormorant'>Today's Special</h1>
+    </div>
+    <div className='app__specialMenu-menu'>
+      <div className='app__specialMenu-menu_wine flex__center'>
+        <p className='app__specialMenu_menu_heading'>Wine & Beer</p>
+        <div className='app__specialMenu_menu_items'>
+          {data.wines.map((wine, index) => (
+            <p>{wine.title}</p>
+          ))}
+
+        </div>
+      </div>
+
+    </div>
   </div>
 );
 
