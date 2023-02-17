3171c65cb87fb8d8b6fe1224b1d4b8c4ad5c9497
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-17 13:24:11 -0500

    render title and subtitle dynamically

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -7,7 +7,11 @@ import './Laurels.css';
 const AwardCard = ({ award: {imgUrl, title, subtitle} }) => (
   <div className='app__laurels_awards-card'>
     <img src={imgUrl} alt="award"/>
+    <div className='app__laurels_awards-card_content'>
+      <p className='p__cormorant' style={{ color: "#DCCA87" }}>{title}</p>
+      <p className='p__cormorant'>{subtitle}</p>
 
+    </div>
   </div>
 );
 
