dd7d2e6032e0f81d985aa2af99441a5dc81559e7
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-17 14:03:28 -0500

    add initial divs with inline styling and setup useRef

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -6,10 +6,22 @@ import { images } from '../../constants';
 import './Gallery.css';
 
 const Gallery = () => {
+  const scrollRef = React.useRef(null)
 
   return (
-    <div>
-      Gallery
+    <div className='app__gallery flex__center'>
+      <div className='app__gallery-content'>
+        <SubHeading title="instagram"/>
+        <h1 className='headtext__cormorant'>Photo Gallery</h1>
+        <p className='p__opensans' style={{ color: '#AAA', marginTop: '2rem' }}>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Volutpat mattis ipsum turpis elit elit scelerisque egestas mu.</p>
+        <button type='button' className='custom__button'>View More</button>
+      </div>
+      <div className='app__gallery-images'>
+        <div className='app__gallery-images_container' ref={scrollRef}>
+
+        </div>
+
+      </div>
     </div>
   )
 };
