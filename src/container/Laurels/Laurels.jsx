3a6751f9a8a0b777cc405f98fadf3f94265f7dd4
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-17 13:16:49 -0500

    add section styling and title to Laurels component

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -5,8 +5,14 @@ import { images, data } from '../../constants';
 import './Laurels.css';
 
 const Laurels = () => (
-  <div>
-    Laurels
+  <div className='app__bg app__wrapper section__padding' id="awards">
+    <div className='app__wrapper_info'>
+      <SubHeading title="Awards & recognition"/>
+      <h1 className='headtext__cormorant'>Our Laurels</h1>
+    </div>
+      <div className='app__wrapper_img'>
+
+    </div>
   </div>
 );
 
