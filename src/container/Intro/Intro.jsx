0c4494146e578823c0a2285c53d63bbb77e54f14
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-17 12:14:39 -0500

    render video and change Intro component from straight render () to {}

--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -4,10 +4,17 @@ import { meal } from '../../constants';
 
 import './Intro.css';
 
-const Intro = () => (
-  <div>
-    Intro
+const Intro = () => {
+  <div className='app__video'>
+    <video 
+      src={meal}
+      ref={vidRef}
+      type="video/mp4"
+      loop
+      controls={false}
+      muted
+      />
   </div>
-);
+};
 
 export default Intro;
