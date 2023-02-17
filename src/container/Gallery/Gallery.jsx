8c54110681e88b1b888809e31adc6985af842d08
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-17 13:56:57 -0500

    add imports to gallery component

--- src/container/Gallery/Gallery.jsx
+++ src/container/Gallery/Gallery.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import { BsInstagram, BsArrowLeftShort, BsArrowRightShort } from 'react-icons/bs';
+import { SubHeading } from '../../components';
+import { images } from '../../constants';
 
 import './Gallery.css';
 
