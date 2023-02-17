7ba2ed27e5c37a4f3466c000f70310e9de2f0e25
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-17 13:52:41 -0500

    add css styling to awards section

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -6,7 +6,7 @@ import './Laurels.css';
 
 const AwardCard = ({ award: {imgUrl, title, subtitle} }) => (
   <div className='app__laurels_awards-card'>
-    <img src={imgUrl} alt="award"/>
+    <img src={imgUrl} alt="awards"/>
     <div className='app__laurels_awards-card_content'>
       <p className='p__cormorant' style={{ color: "#DCCA87" }}>{title}</p>
       <p className='p__cormorant'>{subtitle}</p>
