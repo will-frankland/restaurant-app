28cf5915d5b07feacfc33e9fd2c3cf7ede0072d2
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-17 13:25:06 -0500

    add G logo to laurels section overlaying on photo

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -10,7 +10,6 @@ const AwardCard = ({ award: {imgUrl, title, subtitle} }) => (
     <div className='app__laurels_awards-card_content'>
       <p className='p__cormorant' style={{ color: "#DCCA87" }}>{title}</p>
       <p className='p__cormorant'>{subtitle}</p>
-
     </div>
   </div>
 );
@@ -26,7 +25,7 @@ const Laurels = () => (
       </div>
     </div>
       <div className='app__wrapper_img'>
-
+        <img src={images.laurels} alt='laurels'/>
     </div>
   </div>
 );
