abd2246e4121d224d19d847d517293987a8b24b8
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-17 13:20:00 -0500

    create AwardCard component and pass props to it - render this in the Laurels component mapping over data

--- src/container/Laurels/Laurels.jsx
+++ src/container/Laurels/Laurels.jsx
@@ -4,11 +4,22 @@ import { images, data } from '../../constants';
 
 import './Laurels.css';
 
+const AwardCard = ({ award: {imgUrl, title, subtitle} }) => (
+  <div className='app__laurels_awards-card'>
+    <img src={imgUrl} alt="award"/>
+
+  </div>
+);
+
 const Laurels = () => (
   <div className='app__bg app__wrapper section__padding' id="awards">
     <div className='app__wrapper_info'>
       <SubHeading title="Awards & recognition"/>
       <h1 className='headtext__cormorant'>Our Laurels</h1>
+      <div app__laurels_awards>
+        {data.awards.map((award) => <AwardCard award={award} key={award.title} />)}
+
+      </div>
     </div>
       <div className='app__wrapper_img'>
 
