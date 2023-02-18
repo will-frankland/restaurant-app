3a8a9b4d910e17f158625363e375c6167c39726c
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-18 03:22:09 -0500

    add placeholder and input to Newsletter for updates

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -4,8 +4,16 @@ import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div>
-    Newsletter
+  <div className='app__newslettr'>
+    <div className='app__newsletter-heading'>
+      <SubHeading title="newsletter"/>
+      <h1 className='headtext__cormorant'>Subscribe to our Newsletter</h1>
+      <p className='p__opensans'>And newver miss our latest updates!</p>
+    </div>
+    <div className='app__newsletter-input flex__center'>
+      <input type="email" placeholder='Enter your email address'/>
+      <button className='customer__button'>Subscribe</button>
+    </div>
   </div>
 );
 
