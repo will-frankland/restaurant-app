ae662044e6095b41c3957ac25ce3afab3ab30821
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-18 14:55:12 -0500

    add div containers to Footer and implement trademark p tag

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -8,6 +8,21 @@ const Footer = () => (
   <div className='app__footer section__padding'>
     <FooterOverlay />
     <Newsletter />
+
+    <div className='app__footer-links'>
+      <div className='app__footer-links_contact'>
+
+      </div>
+      <div className='app__footer-links_logo'>
+
+      </div>
+      <div className='app__footer-links_work'>
+
+      </div>
+    </div>
+    <div className='footer__copyright'>
+      <p className='p__opensans'>2023 Gerícht. All Rights reserved.</p>
+    </div>
   </div>
 );
 
