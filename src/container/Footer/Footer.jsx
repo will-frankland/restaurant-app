9d9747b632c76465d4f3623a8513bbfd8dbac67f
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-18 15:03:41 -0500

    import images and add spoon image

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { FooterOverlay, Newsletter } from '../../components';
 import { FiFacebook, FiTwitter, FiInstagram } from 'react-icons/fi';
+import { images } from '../../constants';
 
 import './Footer.css';
 
@@ -17,7 +18,9 @@ const Footer = () => (
         <p className='p__opensans'>+1 212-555-1230</p>
       </div>
       <div className='app__footer-links_logo'>
-
+        <img src={images.gericht} alt="footer_logo"/>
+        <p className='p__opensans'>"The best way to find yourself is to lose yourself in the service of others.”</p>
+        <img src={images.spoon} alt="spoon" className='spoon__img' style={{ marginTop: 15 }}/>
       </div>
       <div className='app__footer-links_work'>
 
