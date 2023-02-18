d8c38c8521de5fac28c17da0809c8080dac05f55
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-18 14:46:01 -0500

    add css styling to heading and button

--- src/components/Footer/Newsletter.jsx
+++ src/components/Footer/Newsletter.jsx
@@ -4,7 +4,7 @@ import SubHeading from '../SubHeading/SubHeading';
 import './Newsletter.css';
 
 const Newsletter = () => (
-  <div className='app__newslettr'>
+  <div className='app__newsletter'>
     <div className='app__newsletter-heading'>
       <SubHeading title="newsletter"/>
       <h1 className='headtext__cormorant'>Subscribe to our Newsletter</h1>
@@ -12,7 +12,7 @@ const Newsletter = () => (
     </div>
     <div className='app__newsletter-input flex__center'>
       <input type="email" placeholder='Enter your email address'/>
-      <button className='customer__button'>Subscribe</button>
+      <button className='custom__button'>Subscribe</button>
     </div>
   </div>
 );
