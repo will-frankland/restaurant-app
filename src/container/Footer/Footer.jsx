cb0ffe7b1b2e91b6dda1892072e9a67f6848df64
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-02-18 15:05:03 -0500

    add social media icons to footer

--- src/container/Footer/Footer.jsx
+++ src/container/Footer/Footer.jsx
@@ -21,6 +21,11 @@ const Footer = () => (
         <img src={images.gericht} alt="footer_logo"/>
         <p className='p__opensans'>"The best way to find yourself is to lose yourself in the service of others.”</p>
         <img src={images.spoon} alt="spoon" className='spoon__img' style={{ marginTop: 15 }}/>
+        <div className='app__footer-links_icons'>
+          <FiFacebook />
+          <FiTwitter />
+          <FiInstagram />
+        </div>
       </div>
       <div className='app__footer-links_work'>
 
